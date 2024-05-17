8efa04379f62755e7bccdae229bfc4be7250fdc8
ShahibHAnik <mdanik@asa.edu>
2024-05-17 02:55:21 -0400

    trying to make calculator

--- src/calculator.java
+++ src/calculator.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class calculator {
+public class calculator {
+    import java.util.Scanner;
+
+    public static void main(String[] args) {
+
+        Scanner input =new Scanner (system.in);
+        int number A = input.nextIn
+    }
 }
